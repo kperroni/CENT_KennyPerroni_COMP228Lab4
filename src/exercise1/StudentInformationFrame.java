b84b190e1f74b07b28d28418f063d90b3a553f8f
kperroni <kenny.perroni@gmail.com>
2017-03-22 13:53:10 -0400

    Finished assignment 4

--- src/exercise1/StudentInformationFrame.java
+++ src/exercise1/StudentInformationFrame.java
@@ -28,82 +28,81 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 
-
 @SuppressWarnings("serial")
 public class StudentInformationFrame extends JFrame implements ActionListener, ItemListener, MouseListener {
 
-	//Declaring label array
+	// Declaring label array
 	private JLabel[] myLabel;
-	//Declaring label names array
+	// Declaring label names array
 	private final String[] names = { "Name:", "Address:", "Province:", "City:", "Postal Code:", "Phone number:",
 			"Email:" };
-	//Declaring text field array
+	// Declaring text field array
 	private JTextField[] myTxt;
-	//Declaring check boxes
+	// Declaring check boxes
 	private final JCheckBox checkStudentCouncil, checkVolunteerWork;
-	//Declaring radio buttons
+	// Declaring radio buttons
 	private final JRadioButton radioComputerScience, radioBusiness;
-	//Declaring radio button group
+	// Declaring radio button group
 	private final ButtonGroup groupStudentMajor;
-	//Declaring combo box to hold string values
+	// Declaring combo box to hold string values
 	private final JComboBox<String> course;
-	//Declaring a JList to display courses selected
+	// Declaring a JList to display courses selected
 	private JList<String> courseList;
-	//Declaring a list model to manipulate the JList element
+	// Declaring a list model to manipulate the JList element
 	private DefaultListModel<String> listModel = new DefaultListModel<String>();
-	//Declaring a button to display the information
+	// Declaring a button to display the information
 	private final JButton display;
-	//Declaring a text area that will contain information about the student
+	// Declaring a text area that will contain information about the student
 	private final JTextArea output;
-	//Declaring scroll pane that will be attached to the text area
+	// Declaring scroll pane that will be attached to the text area
 	private final JScrollPane scroll;
-	//Declaring panels to organize the content
+	// Declaring panels to organize the content
 	private final JPanel west, east, eastNorth, eastCenter, south, southBox, center;
 
-	//Constructor
-	
+	// Constructor
+
 	public StudentInformationFrame() {
-		
-		//Setting title
+
+		// Setting title
 		super("Student Information Form");
 
-		//Creating check boxes
+		// Creating check boxes
 		checkStudentCouncil = new JCheckBox("Student Council");
 		checkVolunteerWork = new JCheckBox("Volunteer Work");
 
-		//Creating radio buttons
+		// Creating radio buttons
 		radioComputerScience = new JRadioButton("Computer Science");
 		radioBusiness = new JRadioButton("Business");
-		
-		//Creating radio button group and adding radio buttons to it
+
+		// Creating radio button group and adding radio buttons to it
 		groupStudentMajor = new ButtonGroup();
 		groupStudentMajor.add(radioComputerScience);
 		groupStudentMajor.add(radioBusiness);
 
-		//Creating and configuring combo box
+		// Creating and configuring combo box
 		course = new JComboBox<String>();
 		course.setMaximumRowCount(3);
 		course.setPreferredSize(new Dimension(250, 60));
 		course.setSize(new Dimension(100, 500));
-		
-		//Creating JList element
+
+		// Creating JList element
 		courseList = new JList<String>(listModel);
 
-		//Creating button to display information
+		// Creating button to display information
 		display = new JButton("Display");
 		display.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 
-		//Creating text area that will hold information
+		// Creating text area that will hold information
 		output = new JTextArea(4, 75);
-		output.setEditable(true);
-		
-		//Creating scroll bar and attaching it to the text area
+		output.setEditable(false);
+
+		// Creating scroll bar and attaching it to the text area
 		scroll = new JScrollPane(output);
 		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scroll.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 
-		//Creating different panels
+		// Creating different panels
 		west = new JPanel();
 		east = new JPanel();
 		eastNorth = new JPanel();
@@ -112,34 +111,34 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 		southBox = new JPanel();
 		center = new JPanel();
 
-		//Setting frame layout
+		// Setting frame layout
 		setLayout(new BorderLayout(5, 5));
 
-		//Setting west panel layout
+		// Setting west panel layout
 		west.setLayout(new GridLayout(7, 1, 3, 3));
-		
-		//Adding panels to the frame
+
+		// Adding panels to the frame
 		add(west, BorderLayout.WEST);
 		add(east, BorderLayout.EAST);
 		add(south, BorderLayout.SOUTH);
 		add(center, BorderLayout.CENTER);
 
-		//Adding panel to south panel
+		// Adding panel to south panel
 		south.add(southBox);
-		
-		//Setting layout to panel
+
+		// Setting layout to panel
 		southBox.setLayout(new BoxLayout(southBox, BoxLayout.Y_AXIS));
-		//Adding button and text area to panel
+		// Adding button and text area to panel
 		southBox.add(display);
 		southBox.add(scroll);
 
-		//Initializing label and text field array
+		// Initializing label and text field array
 		myLabel = new JLabel[names.length];
 		myTxt = new JTextField[names.length];
 
-		//Loop to create labels and text fields
-		//Adding labels and text fields inside the loop
-		
+		// Loop to create labels and text fields
+		// Adding labels and text fields inside the loop
+
 		for (int i = 0; i < names.length; i++) {
 
 			myLabel[i] = new JLabel(names[i]);
@@ -149,31 +148,31 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 
 		}
 
-		//Setting layout to panel
+		// Setting layout to panel
 		center.setLayout(new BorderLayout(2, 2));
-		//Adding check boxes to panel
+		// Adding check boxes to panel
 		center.add(checkStudentCouncil, BorderLayout.NORTH);
 		center.add(checkVolunteerWork, BorderLayout.CENTER);
 
-		//Setting layout to panel
+		// Setting layout to panel
 		east.setLayout(new BorderLayout(2, 40));
-		//Adding panel inside another panel
+		// Adding panel inside another panel
 		east.add(eastNorth, BorderLayout.NORTH);
-		//Setting layout to panel
+		// Setting layout to panel
 		eastNorth.setLayout(new FlowLayout());
-		//Adding radio buttons to panel
+		// Adding radio buttons to panel
 		eastNorth.add(radioComputerScience);
 		eastNorth.add(radioBusiness);
 
-		//Adding panel inside another panel
+		// Adding panel inside another panel
 		east.add(eastCenter, BorderLayout.CENTER);
-		//Setting layout to panel
+		// Setting layout to panel
 		eastCenter.setLayout(new GridLayout(2, 1, 0, 0));
-		//Adding combo box and JList to panel
+		// Adding combo box and JList to panel
 		eastCenter.add(course);
 		eastCenter.add(courseList);
-		
-		//Adding listener to combo box, button, and radio buttons
+
+		// Adding listener to combo box, button, and radio buttons
 		course.addItemListener(this);
 		display.addMouseListener(this);
 		radioBusiness.addActionListener(this);
@@ -181,21 +180,20 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 
 	}
 
-	
-	//Listener for radio buttons
+	// Listener for radio buttons
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 
 		JRadioButton auxRadio = (JRadioButton) e.getSource();
 
-		//If computer science radio button is selected
+		// If computer science radio button is selected
 		if (auxRadio.getText() == "Computer Science") {
 
-			//Clearing combo box and JList
+			// Clearing combo box and JList
 			course.removeAllItems();
 			listModel.removeAllElements();
-			//Adding elements to combo box
+			// Adding elements to combo box
 			course.addItem("");
 			course.addItem("PHP");
 			course.addItem("Java");
@@ -204,14 +202,14 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 
 		}
 
-		//If business radio button is selected
-		
-		else {			
-			
-			//Clearing combo box and JList
+		// If business radio button is selected
+
+		else {
+
+			// Clearing combo box and JList
 			course.removeAllItems();
 			listModel.removeAllElements();
-			//Adding elements to combo box
+			// Adding elements to combo box
 			course.addItem("");
 			course.addItem("Quality Service");
 			course.addItem("Statistics");
@@ -222,16 +220,15 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 
 	}
 
-	
-	//Listener for combo box
+	// Listener for combo box
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		// TODO Auto-generated method stub
 
-		//When an item is selected
+		// When an item is selected
 		if (e.getStateChange() == ItemEvent.SELECTED) {
-			
-			//If the selected item is not in the list, it is added
+
+			// If the selected item is not in the list, it is added
 			if (!listModel.contains(e.getItem().toString()))
 				listModel.addElement(e.getItem().toString());
 
@@ -239,17 +236,18 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 
 	}
 
-	//Listener for display button
+	// Listener for display button
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 
-		//Creating courses, activities and program variables
+		// Creating courses, activities and program variables
 		String myCourses = "";
 		String otherActivities = "";
 		String program = "";
 
-		//Checking which check boxes were selected and assigning the value to otherActivities variable
+		// Checking which check boxes were selected and assigning the value to
+		// otherActivities variable
 		if (checkStudentCouncil.isSelected() && checkVolunteerWork.isSelected())
 
 			otherActivities = checkStudentCouncil.getText() + "-" + checkVolunteerWork.getText();
@@ -266,7 +264,8 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 
 			otherActivities = "No activities";
 
-		//Checking which radio button was selected and assigning the value to program variable
+		// Checking which radio button was selected and assigning the value to
+		// program variable
 		if (radioBusiness.isSelected())
 
 			program = radioBusiness.getText();
@@ -278,28 +277,28 @@ public class StudentInformationFrame extends JFrame implements ActionListener, I
 		else
 			program = "No program selected";
 
-		//Clearing text area
+		// Clearing text area
 		output.setText("");
-		
-		//Getting courses from JList and adding the values to myCourses String
+
+		// Getting courses from JList and adding the values to myCourses String
 		for (int i = 0; i < listModel.size(); i++) {
 
 			myCourses += listModel.getElementAt(i) + "\n";
 
 		}
-		
-		//Printing student information in text area
+
+		// Printing student information in text area
 		output.setText(String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s%n%s%s", myTxt[0].getText(), myTxt[1].getText(),
 				myTxt[2].getText(), myTxt[3].getText(), myTxt[4].getText(), myTxt[5].getText(), myTxt[6].getText(),
 				otherActivities, program, "Courses:", myCourses));
 
-		//Setting text area cursor at the beginning
+		// Setting text area cursor at the beginning
 		output.setCaretPosition(0);
 
 	}
-	
-//Not implemented methods	
-	
+
+	// Not implemented methods
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
--- src/exercise2/StudentInformationDemoFX.java
+++ src/exercise2/StudentInformationDemoFX.java
@@ -1,18 +1,13 @@
 package exercise2;
 
-import java.awt.Dimension;
-import java.awt.Panel;
-
-import javax.swing.ButtonGroup;
-import javax.swing.DefaultListModel;
-
-import javax.swing.ScrollPaneConstants;
-
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -25,146 +20,264 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class StudentInformationDemoFX extends Application{
+public class StudentInformationDemoFX extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		//Declaring label array
-		
+
+		// Declaring grid panes
 		GridPane root = new GridPane();
 		root.setAlignment(Pos.TOP_LEFT);
 		root.setHgap(5);
 		root.setVgap(5);
-		
+
 		GridPane leftPane = new GridPane();
 		leftPane.setAlignment(Pos.TOP_LEFT);
 		leftPane.setHgap(5);
 		leftPane.setVgap(5);
-		
+
 		GridPane rightPane = new GridPane();
 		rightPane.setAlignment(Pos.TOP_LEFT);
-		rightPane.setHgap(5);
+		rightPane.setHgap(2);
 		rightPane.setVgap(5);
-		
-		GridPane centerPane = new GridPane();
-		centerPane.setAlignment(Pos.BOTTOM_CENTER);
-		centerPane.setHgap(5);
-		centerPane.setVgap(5);
-		
-		FlowPane flowPane = new FlowPane();
-		flowPane.setPadding(new Insets(11,12,13,14));
 
-		
-		 Label[] myLabel;
-		//Declaring label names array
-		  String[] names = { "Name:", "Address:", "Province:", "City:", "Postal Code:", "Phone number:",
-				"Email:" };
-		//Declaring text field array
-		 TextField[] myTxt;
-		//Declaring check boxes
-		  CheckBox checkStudentCouncil, checkVolunteerWork;
-		//Declaring radio buttons
-		  RadioButton radioComputerScience, radioBusiness;
-		//Declaring radio button group
-		  ToggleGroup  groupStudentMajor;
-		//Declaring combo box to hold string values
-		  ComboBox<String> course;
-		//Declaring a JList to display courses selected
-		 ListView<String> courseList;
-		//Declaring a list model to manipulate the JList element
-		 ObservableList<String> listModel =  FXCollections.observableArrayList();
-		//Declaring a button to display the information
-		  Button display;
-		//Declaring a text area that will contain information about the student
-		  TextArea output;
-		//Declaring scroll pane that will be attached to the text area
-		  ScrollPane scroll;
-		  
-		//Creating check boxes
-			checkStudentCouncil = new CheckBox("Student Council");
-			checkVolunteerWork = new CheckBox("Volunteer Work");
+		GridPane rightLeftContainer = new GridPane();
+		rightLeftContainer.setAlignment(Pos.TOP_LEFT);
+		rightLeftContainer.setHgap(2);
+		rightLeftContainer.setVgap(5);
 
-			//Creating radio buttons
-			radioComputerScience = new RadioButton("Computer Science");
-			radioBusiness = new RadioButton("Business");
-			
-			//Creating radio button group and adding radio buttons to it
-			groupStudentMajor = new ToggleGroup();
-			
-			radioComputerScience.setToggleGroup(groupStudentMajor);
-			radioBusiness.setToggleGroup(groupStudentMajor);
+		GridPane bottomPane = new GridPane();
+		bottomPane.setAlignment(Pos.BOTTOM_CENTER);
+		bottomPane.setHgap(5);
+		bottomPane.setVgap(5);
 
-			//Creating and configuring combo box
-			course = new ComboBox<String>();
-			course.setVisibleRowCount(3);
-			course.setPrefSize(248, 60);
-			
-			//Creating ListView element
-			courseList = new ListView<String>();
-			courseList.setPrefSize(1, 100);
-			courseList.setPrefWidth(1);
+		GridPane radioPane = new GridPane();
+		radioPane.setAlignment(Pos.BOTTOM_CENTER);
+		radioPane.setHgap(5);
+		radioPane.setVgap(5);
 
-			//Creating button to display information
-			display = new Button("Display");
-			GridPane.setHalignment(display, HPos.CENTER);
+		// Declaring label array
+		Label[] myLabel;
+		// Declaring label names array
+		String[] names = { "Name:", "Address:", "Province:", "City:", "Postal Code:", "Phone number:", "Email:" };
+		// Declaring text field array
+		TextField[] myTxt;
+		// Declaring check boxes
+		CheckBox checkStudentCouncil, checkVolunteerWork;
+		// Declaring radio buttons
+		RadioButton radioComputerScience, radioBusiness;
+		// Declaring radio toggle group
+		ToggleGroup groupStudentMajor;
+		// Declaring combo box to hold string values
+		ComboBox<String> course;
+		// Declaring a list view to display courses selected
+		ListView<String> courseList;
+		// Declaring a list view model to manipulate the List element
+		ObservableList<String> listModel = FXCollections.observableArrayList();
+		// Values for list view
+		ObservableList<String> computerScienceCourses = FXCollections.observableArrayList("C#", "Java", "C++", "PHP",
+				"Python");
 
-			//Creating text area that will hold information
-			output = new TextArea();
-			output.setPrefColumnCount(75);
-			output.setPrefRowCount(4);
-			output.setEditable(true);
-			
-			//Creating scroll bar and attaching it to the text area
-			scroll = new ScrollPane(output);
-			scroll.setVbarPolicy(ScrollBarPolicy.ALWAYS);
-			scroll.setHbarPolicy(ScrollBarPolicy.ALWAYS);
-			GridPane.setHalignment(scroll, HPos.CENTER);
-		  
-		//Initializing label and text field array
-		  myLabel = new Label[names.length];
-		  myTxt = new TextField[names.length];
-		  
-		  for (int i = 0; i < names.length; i++) {
+		ObservableList<String> businessCourses = FXCollections.observableArrayList("Statistics", "Business Management",
+				"Human Resources", "Communications", "English");
 
-				myLabel[i] = new Label(names[i]);
-				myTxt[i] = new TextField();
-				leftPane.add(myLabel[i], 0, i);
-				leftPane.add(myTxt[i], 35, i);
+		// Declaring a button to display the information
+		Button display;
+		// Declaring a text area that will contain information about the student
+		TextArea output;
+		// Declaring scroll pane that will be attached to the text area
+		ScrollPane scroll;
+
+		// Creating check boxes
+		checkStudentCouncil = new CheckBox(String.format("%-30s", "Student Council"));
+		checkVolunteerWork = new CheckBox("Volunteer Work");
+
+		// Creating radio buttons
+		radioComputerScience = new RadioButton("Computer Science");
+		radioBusiness = new RadioButton("Business");
+
+		// Creating radio button group and adding radio buttons to it
+		groupStudentMajor = new ToggleGroup();
+
+		radioComputerScience.setToggleGroup(groupStudentMajor);
+		radioBusiness.setToggleGroup(groupStudentMajor);
+
+		// Creating and configuring combo box
+		course = new ComboBox<String>();
+		course.setVisibleRowCount(3);
+		course.setPrefSize(300, 60);
+
+		// Creating ListView element
+		courseList = new ListView<String>(listModel);
+		courseList.setPrefSize(1, 100);
+
+		// Creating button to display information
+		display = new Button("Display");
+		GridPane.setHalignment(display, HPos.CENTER);
+
+		// Creating text area that will hold information
+		output = new TextArea();
+		output.setPrefColumnCount(75);
+		output.setPrefRowCount(6);
+		output.setEditable(false);
+
+		// Creating scroll bar and attaching it to the text area
+		scroll = new ScrollPane(output);
+		scroll.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		scroll.setHbarPolicy(ScrollBarPolicy.ALWAYS);
+		GridPane.setHalignment(scroll, HPos.CENTER);
+
+		// Initializing label and text field array
+		myLabel = new Label[names.length];
+		myTxt = new TextField[names.length];
+
+		for (int i = 0; i < names.length; i++) {
+
+			myLabel[i] = new Label(String.format("%-30s", names[i]));
+			myTxt[i] = new TextField();
+			leftPane.add(myLabel[i], 0, i);
+			leftPane.add(myTxt[i], 5, i);
+
+		}
+
+		// Adding elements to left pane
+		leftPane.add(checkVolunteerWork, 6, 1);
+		leftPane.add(checkStudentCouncil, 6, 4);
+
+		// Adding radio buttons to radio pane
+		radioPane.add(radioComputerScience, 0, 0);
+		radioPane.add(radioBusiness, 1, 0);
+
+		// Adding elements to right pane
+		rightPane.add(radioPane, 7, 0);
+		rightPane.add(course, 7, 2);
+		rightPane.add(courseList, 7, 3);
+
+		// Adding elements to bottom pane
+		bottomPane.add(display, 0, 6);
+		bottomPane.add(scroll, 0, 7);
+
+		rightLeftContainer.add(leftPane, 0, 0);
+		rightLeftContainer.add(rightPane, 1, 0);
+
+		// Adding elements to root pane
+		root.add(rightLeftContainer, 0, 0);
+		root.add(bottomPane, 0, 1);
+
+		// Adding listener to combo box, button, and radio buttons
+		groupStudentMajor.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+
+			public void changed(ObservableValue<? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) {
+
+				RadioButton checked = (RadioButton) groupStudentMajor.getSelectedToggle();
+
+				if (checked.getText().equals("Computer Science")) {
+					course.getSelectionModel().clearSelection();
+					listModel.clear();
+					course.setItems(computerScienceCourses);
+				}
+
+				else {
+					if (checked.getText().equals("Business")) {
+						course.getSelectionModel().clearSelection();
+						listModel.clear();
+						course.setItems(businessCourses);
+					}
+				}
 
 			}
-  
-		 leftPane.add(checkVolunteerWork, 40, 1);
-		 leftPane.add(checkStudentCouncil, 40, 4);
-		 
-		 flowPane.getChildren().addAll(radioComputerScience, radioBusiness);
-	
-		 rightPane.add(flowPane, 120, 0);
-		 
-		 
-		 rightPane.add(course, 120, 2);
-		 rightPane.add(courseList, 120, 3);
-		 
-		 centerPane.add(display, 0, 50);
-		 centerPane.add(scroll, 0, 51);
-		 
-		 root.getChildren().add(leftPane);
-		 root.getChildren().add(rightPane);
-		 root.getChildren().add(centerPane);
-		 
-		  Scene scene = new Scene(root, 850, 400);
-		  
-			primaryStage.setTitle("Student Information Form");
-			primaryStage.setScene(scene);
-			primaryStage.show();
+		});
+
+		course.valueProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> arg0, String t1, String t2) {
+				// TODO Auto-generated method stub
+				if (!listModel.contains(t2) && t2 != null) {
+					listModel.add(t2);
+				}
+			}
+		});
+
+		display.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+
+				// Creating courses, activities and program variables
+				String myCourses = "";
+				String otherActivities = "";
+				String program = "";
+
+				// Checking which check boxes were selected and assigning the
+				// value to otherActivities variable
+				if (checkStudentCouncil.isSelected() && checkVolunteerWork.isSelected())
+
+					otherActivities = checkStudentCouncil.getText().trim() + "-" + checkVolunteerWork.getText().trim();
+
+				else if (checkStudentCouncil.isSelected() && !checkVolunteerWork.isSelected())
+
+					otherActivities = checkStudentCouncil.getText().trim();
+
+				else if (checkVolunteerWork.isSelected() && !checkStudentCouncil.isSelected())
+
+					otherActivities = checkVolunteerWork.getText().trim();
+
+				else if (!checkStudentCouncil.isSelected() && !checkVolunteerWork.isSelected())
+
+					otherActivities = "No activities";
+
+				// Checking which radio button was selected and assigning the
+				// value to program variable
+				if (radioBusiness.isSelected())
+
+					program = radioBusiness.getText();
+
+				else if (radioComputerScience.isSelected())
+
+					program = radioComputerScience.getText();
+
+				else
+					program = "No program selected";
+
+				// Clearing text area
+				output.setText("");
+
+				// Getting courses from JList and adding the values to myCourses
+				// String
+				for (int i = 0; i < listModel.size(); i++) {
+
+					myCourses += listModel.get(i) + "\n";
+
+				}
+
+				// Printing student information in text area
+				output.setText(String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s%n%s%n%s", myTxt[0].getText(),
+						myTxt[1].getText(), myTxt[2].getText(), myTxt[3].getText(), myTxt[4].getText(),
+						myTxt[5].getText(), myTxt[6].getText(), otherActivities, program, "Courses:", myCourses));
+
+				// Setting text area cursor at the beginning
+				output.positionCaret(0);
+
+			}
+
+		});
+
+		// Creating scene
+		Scene scene = new Scene(root, 850, 400);
+
+		// Configuring stage
+		primaryStage.setTitle("Student Information Form");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
